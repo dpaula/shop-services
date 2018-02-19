ba6c28e756dcde02a4d73d275789d9e93d5d61c1
dpaula <fernando.dpaula@gmail.com>
2018-02-19 09:48:51 -0300

    incluido o post para o servidor

--- src/main/java/br/com/dpaula/shop/resource/CarrinhoResource.java
+++ src/main/java/br/com/dpaula/shop/resource/CarrinhoResource.java
@@ -1,10 +1,13 @@
 package br.com.dpaula.shop.resource;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+
+import com.thoughtworks.xstream.XStream;
 
 import br.com.dpaula.shop.dao.CarrinhoDAO;
 import br.com.dpaula.shop.modelo.Carrinho;
@@ -25,6 +28,15 @@ public class CarrinhoResource {
 		Carrinho carrinho = new CarrinhoDAO().busca(id);
 
 		return carrinho.toXML();
+	}
+
+	@POST
+	@Produces(MediaType.APPLICATION_XML)
+	public String adiciona(String conteudo) {
+		Carrinho carrinho = (Carrinho) new XStream().fromXML(conteudo);
+		new CarrinhoDAO().adiciona(carrinho);
+		return "<status>sucesso</status>";
+
 	}
 
 }
