3ac3ae73f1eebfc1578d0d4ab6b015563a6a9cfd
dpaula <fernando.dpaula@gmail.com>
2018-02-19 08:24:21 -0300

    criado um servidor Grizzly para exportar uma URI com a representação do meu objeto no navegador

--- src/main/java/br/com/dpaula/shop/Servidor.java
+++ src/main/java/br/com/dpaula/shop/Servidor.java
@@ -1,9 +1,23 @@
 package br.com.dpaula.shop;
 
+import java.io.IOException;
+import java.net.URI;
+
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
+import org.glassfish.jersey.server.ResourceConfig;
+
 public class Servidor {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 
+		URI uri = URI.create("http://localhost:8787/");
+		ResourceConfig config = new ResourceConfig().packages("br.com.dpaula.shop");
+		HttpServer server = GrizzlyHttpServerFactory.createHttpServer(uri, config);
+
+		System.out.println("Servidor rodando");
+		System.in.read();
+		server.stop();
 	}
 
 }
